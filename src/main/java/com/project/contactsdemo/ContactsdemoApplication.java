a7649d2d4c4c04980e57e81e95f95f59921a0ff6
AYSE CEREN COBAN <16381136672@GGM.BIM>
2025-02-12 16:22:26 +0300

    firstcommit

---
+++ src/main/java/com/project/contactsdemo/ContactsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.project.contactsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/contactsdemo/ContactsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.contactsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
