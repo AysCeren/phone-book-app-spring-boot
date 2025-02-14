58905c7003bdc1844e6074f582a81a2e33d429b5
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-14 12:30:49 +0300

    Service'e: updateContact ve deleteContact eklendi Fakat: getAllContacts ve getAllPersonWithContacts tamamlanmadı

--- src/main/java/com/project/contactsdemo/service/PersonContactService.java
+++ src/main/java/com/project/contactsdemo/service/PersonContactService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonContactService {
@@ -24,6 +25,7 @@ public class PersonContactService {
         this.personRepository = personRepository;
     }
 
+    //TODO: Transactional'lara yalıtım ve yayılma ekleyelim.
     @Transactional
     public void savePerson(PersonDTO savePersonRequestDto) {
         Person person = fromPersonDTOToEntity(savePersonRequestDto);
@@ -51,8 +53,45 @@ public class PersonContactService {
         this.contactsRepository.save(contact);
     }
 
+    @Transactional
+    public void updateContact(ContactDTO updatedContactDTO) {
+        Contact updatedContact = getContact(updatedContactDTO);
+        this.contactsRepository.save(updatedContact);
+    }
 
+    public void deleteContact(ContactDTO deletedContactDTO) {
+        Contact deletedContact = getContact(deletedContactDTO);
+        deletedContact.setStatus(0); //0: deleted, 1: exist
+        contactsRepository.save(deletedContact);
+    }
 
+    public Contact getContact(ContactDTO contactDTO) {
+        Contact contact = fromContactDTOToEntity(contactDTO);
+        assert contact != null; //TODO: Burada gelen dto'nun boş olup olmadığı kontrol edilecek mi?
+        Optional<Contact> deletedContact = contactsRepository.findById(contact.getId());
+        if (deletedContact.isEmpty()) {
+            throw new ContactNotFoundException("No contact found");
+        }
+        else{
+            return contact;
+        }
+    }
+    /*
+    @Transactional
+    public List<ContactDTO> getAllContacts(Long personId) {
+        //TODO: Optional Class olarak döndüreceğiz
+        List<Contact> contactList = new ArrayList<>(contactsRepository.findByPersonId(personId));
+        if (personList.isEmpty()) {
+            throw new ContactNotFoundException("No contacts found");
+        } else {
+            List<PersonDTO> requestDtoList = new ArrayList();
+            for(Person person : personList) {
+                requestDtoList.add(fromPersonToPersonDto(person));
+            }
+            return requestDtoList;
+        }
+    }
+     */
     //TODO: Work on this with mapStructure
     //TODO: Can we work with Collections
     private Person fromPersonDTOToEntity(PersonDTO requestDto) {
