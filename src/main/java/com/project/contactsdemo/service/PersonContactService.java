65d453f98acf31e98ee21993c4016a8ae670e921
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-14 15:12:38 +0300

    import org.springframework.transaction.annotation.Transactional; eklendi ve Transactional içinceki propagation dolduruldu.

--- src/main/java/com/project/contactsdemo/service/PersonContactService.java
+++ src/main/java/com/project/contactsdemo/service/PersonContactService.java
@@ -2,13 +2,15 @@ package com.project.contactsdemo.service;
 
 import com.project.contactsdemo.entity.Contact;
 import com.project.contactsdemo.entity.Person;
-import com.project.contactsdemo.exception.ContactNotFoundException;
+import com.project.contactsdemo.exception.TrainingException;
 import com.project.contactsdemo.repository.ContactsRepository;
 import com.project.contactsdemo.repository.PersonRepository;
 import com.project.contactsdemo.requestdto.ContactDTO;
 import com.project.contactsdemo.requestdto.PersonDTO;
-import jakarta.transaction.Transactional;
+//import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,18 +29,19 @@ public class PersonContactService {
     }
 
     //TODO: Transactional'lara yalıtım ve yayılma ekleyelim.
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void savePerson(PersonDTO savePersonRequestDto) {
         Person person = fromPersonDTOToEntity(savePersonRequestDto);
        this.personRepository.save(person);
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    //because it is read-only method, I want it run as non-transactional
     public List<PersonDTO> getAllPerson() {
         //TODO: Optional Class olarak döndüreceğiz
         List<Person> personList = new ArrayList<>(personRepository.findAll());
         if (personList.isEmpty()) {
-            throw new ContactNotFoundException("No contacts found");
+            throw new TrainingException("No contacts found");
         } else {
            List<PersonDTO> requestDtoList = new ArrayList();
            for(Person person : personList) {
@@ -48,18 +51,19 @@ public class PersonContactService {
         }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void saveContact(ContactDTO saveContactRequestDto) {
         Contact contact = fromContactDTOToEntity(saveContactRequestDto);
         this.contactsRepository.save(contact);
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void updateContact(ContactDTO updatedContactDTO) {
         Contact updatedContact = getContact(updatedContactDTO);
         this.contactsRepository.save(updatedContact);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public void deleteContact(ContactDTO deletedContactDTO) {
         Contact deletedContact = getContact(deletedContactDTO);
         deletedContact.setStatus(0); //0: deleted, 1: exist
@@ -71,7 +75,7 @@ public class PersonContactService {
         assert contact != null; //TODO: Burada gelen dto'nun boş olup olmadığı kontrol edilecek mi?
         Optional<Contact> deletedContact = contactsRepository.findById(contact.getId());
         if (deletedContact.isEmpty()) {
-            throw new ContactNotFoundException("No contact found");
+            throw new TrainingException("No contact found");
         }
         else{
             return contact;
@@ -94,7 +98,7 @@ public class PersonContactService {
     }
      */
     //TODO: Work on this with mapStructure
-    //TODO: Can we work with Collections
+    //TODO: Can we work with Collections?
     private Person fromPersonDTOToEntity(PersonDTO requestDto) {
         return null;
     }
@@ -103,6 +107,7 @@ public class PersonContactService {
         return null;
     }
 
+    //TODO: Eğer burada ResponseDTO diye ayrı bir DTO class oluşturacaksa Entity'yi yanıt olarak o class'a döndüreceğiz (bu iki method için)
     private PersonDTO fromPersonToPersonDto(Person person) {
         return null;
     }
