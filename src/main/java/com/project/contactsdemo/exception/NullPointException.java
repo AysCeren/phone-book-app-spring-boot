cd00dba74ea26387eae242ee8d51d30e2379a7de
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-18 17:32:39 +0300

    Exception düzenlenmesine başlandı.

---
+++ src/main/java/com/project/contactsdemo/exception/NullPointException.java
@@ -0,0 +1,7 @@
+package com.project.contactsdemo.exception;
+
+public class NullPointException extends RuntimeException {
+    public NullPointException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/project/contactsdemo/exception/TrainingException.java
+++
@@ -1,8 +0,0 @@
-package com.project.contactsdemo.exception;
-
-public class TrainingException extends RuntimeException {
-    public TrainingException(String message) {
-        super(message);
-    }
-
-}
