a81ec57e141312d6979fad705406d6e616d3d897
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-21 17:26:17 +0300

    NoDataFoundExcepiton'ı kontrol edildi mapPersonIdToPerson yazıldı. (saveContact methodu içinde kullanılmak üzere)

--- src/main/java/com/project/contactsdemo/service/PersonContactService.java
+++ src/main/java/com/project/contactsdemo/service/PersonContactService.java
@@ -2,14 +2,17 @@ package com.project.contactsdemo.service;
 
 import com.project.contactsdemo.entity.Contact;
 import com.project.contactsdemo.entity.Person;
-import com.project.contactsdemo.exception.TrainingException;
-import com.project.contactsdemo.repository.ContactsRepository;
+import com.project.contactsdemo.exception.NoDataFoundException;
+import com.project.contactsdemo.repository.ContactRepository;
 import com.project.contactsdemo.repository.PersonRepository;
 import com.project.contactsdemo.requestdto.ContactRequestDTO;
+import com.project.contactsdemo.requestdto.ContactResponseDTO;
 import com.project.contactsdemo.requestdto.PersonRequestDTO;
 import com.project.contactsdemo.mapper.*;
 //import jakarta.transaction.Transactional;
 import com.project.contactsdemo.requestdto.PersonResponseDTO;
+import jdk.jfr.Name;
+import org.mapstruct.Named;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
@@ -24,13 +27,13 @@ import java.util.stream.Collectors;
 public class PersonContactService {
 
     private final PersonRepository personRepository;
-    private final ContactsRepository contactsRepository;
+    private final ContactRepository contactsRepository;
     private final PersonMapper personMapper;
     private final ContactMapper contactMapper;
 
     //Constructor injection for all
     @Lazy
-    public PersonContactService(ContactsRepository contactsRepository, PersonRepository personRepository,PersonMapper personMapper, ContactMapper contactMapper) {
+    public PersonContactService(ContactRepository contactsRepository, PersonRepository personRepository, PersonMapper personMapper, ContactMapper contactMapper) {
         this.contactsRepository = contactsRepository;
         this.personRepository = personRepository;
         this.personMapper = personMapper;
@@ -46,16 +49,16 @@ public class PersonContactService {
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     //because it is read-only method, I want it run as non-transactional
-    public List<PersonResponseDTO> getAllPerson() {
-        //TODO: Optional Class olarak döndüreceğiz
-        List<Person> personList = new ArrayList<>(personRepository.findAll());
+    public List<PersonResponseDTO> getAllPerson() {List<Person> personList = new ArrayList<>(personRepository.findAll());
         if (personList.isEmpty()) {
-            throw new TrainingException("No contacts found");
+            throw new NoDataFoundException("There is no person found");
         }
+        //for-each ile tarama
         return personList.stream().map(personMapper::fromPersonToPersonResponseDto).collect(Collectors.toList());
     }
 
-    public List<Contact> getAllContact(Long personId){
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public List<ContactResponseDTO> getAllContactsOfPerson(Long personId){
         /*Optional<Person> person = personRepository.findById(personId);
         if (person.isPresent()) {
             List<Contact> contactList = person.get().getContacts();
@@ -74,51 +77,59 @@ public class PersonContactService {
             throw new TrainingException("No contacts found");
         }
         return contactList;
-
          */
-        List<Contact> contactsForPerson = new ArrayList<>(contactsRepository.findAllContactsForPerson(personId));
+        List<Contact> contactsForPerson = new ArrayList<>(contactsRepository.findContactsByPersonId(personId));
         if (contactsForPerson.isEmpty()) {
-            throw new TrainingException("No contacts found");
+            throw new NoDataFoundException("No contacts found for person id: " + personId);
         }
-        return contactsForPerson;
+        return contactsForPerson.stream().map(contactMapper::fromContactEntityToContactResponseDTO).collect(Collectors.toList());
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveContact(ContactRequestDTO saveContactRequestDto) {
+        //öncelikle gelen contact'ın personId'sine bakalım
         Contact contact = contactMapper.fromContactRequestDTOToContactEntity(saveContactRequestDto);
         this.contactsRepository.save(contact);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void updateContact(ContactRequestDTO updatedContactRequestDTO) {
-        Contact contact = contactMapper.fromContactRequestDTOToContactEntity(updatedContactRequestDTO);
-        //check for the existence
-        if(!contactsRepository.existsById(contact.getId())){
-            throw new TrainingException("No such a contact to update");
+        Optional<Contact> contact = contactsRepository.findById(contactMapper.fromContactRequestDTOToContactEntity(updatedContactRequestDTO).getId());
+        //TODO: Maybe using existById() is a better implementation.
+        if(contact.isPresent()) {
+            throw new NoDataFoundException("No such a contact to update");
         }
         else{
-            contact.setName(updatedContactRequestDTO.getName());
-            contact.setPhoneNo(updatedContactRequestDTO.getPhoneNumber());
-            contactsRepository.save(contact);
+            contact.get().setName(updatedContactRequestDTO.getName());
+            contact.get().setPhoneNo(updatedContactRequestDTO.getPhoneNumber());
+            contactsRepository.save(contact.get());
         }
     }
-
     @Transactional(propagation = Propagation.REQUIRED)
     public void deleteContact(ContactRequestDTO deletedContactRequestDTO) {
         Contact deletedContact = contactMapper.fromContactRequestDTOToContactEntity(deletedContactRequestDTO);
         if(!contactsRepository.existsById(deletedContact.getId())){
-            throw new TrainingException("No such a contact to delete");
+            throw new NoDataFoundException("No such a contact to delete");
         }else {
             deletedContact.setStatus(0); //0: deleted, 1: exist
             contactsRepository.save(deletedContact);
         }
     }
-
-    public List<Person> getAllPersonWithContacts(){
+    @Transactional(propagation = Propagation.REQUIRED)
+    public List<PersonResponseDTO> getAllPersonWithContacts(){
         List<Person> personListWithContacts = new ArrayList<>(personRepository.findAllWithContacts());
         if (personListWithContacts.isEmpty()) {
-            throw new TrainingException("No contacts found");
+            throw new NoDataFoundException("No contacts found");
         }else
-            return personListWithContacts;
+            return personListWithContacts.stream().map(personMapper::fromPersonToPersonResponseDto).collect(Collectors.toList());
+    }
+
+    @Named("mapPersonIdToPerson")
+    public Person mapPersonIdToPerson(Long personId) {
+        if (personId == null) {
+            return null;
+        }
+        return personRepository.findById(personId).orElseThrow(() ->
+                new RuntimeException("No such a person: " + personId));
     }
 }
