d64489713aee5f2d234688a36f435bcc2020d7f2
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-21 17:24:40 +0300

    getAllContactsOfPerson ve getAllPersonWithContacts yeniden yazıldı

--- src/main/java/com/project/contactsdemo/controller/PersonContactController.java
+++ src/main/java/com/project/contactsdemo/controller/PersonContactController.java
@@ -9,17 +9,16 @@ package com.project.contactsdemo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-        import com.project.contactsdemo.entity.Contact;
-        import com.project.contactsdemo.requestdto.ContactRequestDTO;
+import com.project.contactsdemo.entity.Contact;
+import com.project.contactsdemo.requestdto.ContactRequestDTO;
+import com.project.contactsdemo.requestdto.ContactResponseDTO;
 import com.project.contactsdemo.requestdto.PersonRequestDTO;
+import com.project.contactsdemo.requestdto.PersonResponseDTO;
 import com.project.contactsdemo.service.PersonContactService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping({"/api"})
@@ -34,59 +33,54 @@ public class PersonContactController {
     //Save operation with POST request
     @RequestMapping(
             method = {RequestMethod.POST},
-            path = {"/contacts1"}
+            path = {"/savePerson"}
     )//model attribute
-    public ResponseEntity<Contact> savePerson(@RequestBody PersonRequestDTO personRequestDTO) {
-        this.personContactService.savePerson(personRequestDTO);
-        return new ResponseEntity(personRequestDTO, HttpStatus.CREATED);
+    public ResponseEntity<PersonResponseDTO> savePerson(@Valid @RequestBody PersonRequestDTO personRequestDTO) {
+        this.personContactService.savePerson(personRequestDTO); //void, no response parameter
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
     }
 
     @RequestMapping(
             method = {RequestMethod.POST},
-            path = {"/contacts2"}
+            path = {"/saveContact"}
     )
-    public ResponseEntity<Contact> saveContact(@RequestBody ContactRequestDTO savedContactRequestDTO) {
-        this.personContactService.saveContact(savedContactRequestDTO);
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity<ContactResponseDTO> saveContact(@RequestBody ContactRequestDTO savedContactRequestDTO) {
+        this.personContactService.saveContact(savedContactRequestDTO); //void, no response parameter
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @RequestMapping(
             method = {RequestMethod.GET},
-            path = {"/contacts3"}
+            path = {"/getAllPerson"}
     )
-    public ResponseEntity<List<Contact>> getAllPerson() {
-        List<Contact> contacts = new ArrayList(this.personContactService.getAllPerson());
-        return new ResponseEntity(contacts, HttpStatus.OK);
+    public ResponseEntity<List<PersonResponseDTO>> getAllPerson() {
+        return new ResponseEntity<>(this.personContactService.getAllPerson(), HttpStatus.OK);
     }
 
 
     @RequestMapping(
             method = {RequestMethod.DELETE},
-            path = {"/contacts4"}
+            path = {"/deleteContact"}
     )
-    public ResponseEntity<Contact> deleteContact(@RequestBody ContactRequestDTO contactRequestDTO) {
-        this.personContactService.deleteContact(contactRequestDTO);
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity<ContactResponseDTO> deleteContact(@Valid @RequestBody ContactRequestDTO contactRequestDTO) {
+        this.personContactService.deleteContact(contactRequestDTO); //void, no response parameter
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping({"/contacts/"})
-    /*
-    @RequestMapping(
-            method = {RequestMethod.PUT},
-            path = {"/contacts5"}
-    )
-
-     */
     public ResponseEntity<Contact> updateContact( @RequestBody ContactRequestDTO contactRequestDTO) {
         this.personContactService.updateContact(contactRequestDTO);
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    /*
-    //TODO: Missing methods: getAllContacts(personId) and getAllPersonWithContacts()
-    public ResponseEntity<List<Contact>> getAllContact(Long personId) {
-        List<Contact> contacts = new ArrayList(this.personContactService.getAllPerson());
+    @GetMapping(path = "/getAllPersonWithContacts")
+    public ResponseEntity<List<PersonResponseDTO>> getAllPersonWithContacts() {
+       return new ResponseEntity<>(personContactService.getAllPersonWithContacts(),HttpStatus.OK);
     }
-    */
 
+    @GetMapping("/getAllContactsOfPerson/{id}")
+    public ResponseEntity<List<ContactResponseDTO>> getAllContactsOfPerson(@PathVariable Long id) {
+        return new ResponseEntity<>(personContactService.getAllContactsOfPerson(id),HttpStatus.OK);
+    }
 }
