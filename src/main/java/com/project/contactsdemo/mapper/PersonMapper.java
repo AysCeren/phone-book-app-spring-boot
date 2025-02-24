147811539604d0331066d23e144e9d7a6e517930
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-24 16:37:18 +0300

    stringToLocalDate() methodu String olarak alınan request birthDate'ini Entity'de LocalDate olarak saklanmak için yazıldı.

--- src/main/java/com/project/contactsdemo/mapper/PersonMapper.java
+++ src/main/java/com/project/contactsdemo/mapper/PersonMapper.java
@@ -5,20 +5,28 @@ import com.project.contactsdemo.requestdto.PersonRequestDTO;
 import com.project.contactsdemo.requestdto.PersonResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Mapper
 public interface PersonMapper {
     //This method takes requestDTO and maps them to Person Entity
     //Note: We do not need to use Mapping, because names are the same.
-    @Mapping(target = "birthDate", source = "birthDate",  dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping(target = "birthDate", source = "birthDate")
     List<Person> fromPersonRequestDTOToPersonEntity(List<PersonRequestDTO> requestDto);
-    @Mapping (target = "birthDate", source = "birthDate",  dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping (target = "birthDate", source = "birthDate",  qualifiedByName = "stringToLocalDate")
     Person fromPersonRequestDTOToPersonEntity(PersonRequestDTO requestDto);
     //This method takes Person Entity and maps them to Person ResponseDTO
-    @Mapping (target = "birthDate", source = "birthDate",  dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping (target = "birthDate", source = "birthDate")
     List<PersonResponseDTO> fromPersonToPersonResponseDto(List<Person> person);
-    @Mapping (target = "birthDate", source = "birthDate",  dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping (target = "birthDate", source = "birthDate")
     PersonResponseDTO fromPersonToPersonResponseDto(Person person);
+
+    @Named("stringToLocalDate")
+    default LocalDate stringToLocalDate(String birthDate) { //it will automatically be used by "fromPersonRequestDTOToPersonEntity"
+        return birthDate != null ? LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("dd-MM-yyyy")) : null;
+    }
 }
