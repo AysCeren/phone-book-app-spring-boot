d39b936cd49ec2f8bb2bfe17153c20133afd80c4
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-26 13:28:04 +0300

    gender için null kontrolü

--- src/main/java/com/project/contactsdemo/validation/GenderValidator.java
+++ src/main/java/com/project/contactsdemo/validation/GenderValidator.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 public class GenderValidator implements ConstraintValidator<ValidGender, Gender> {
     @Override
     public boolean isValid(Gender gender, ConstraintValidatorContext constraintValidatorContext) {
+        if (gender == null) {
+            return false; // Or true if null is allowed
+        }
         return Arrays.stream(Gender.values())
                 .anyMatch(validGender -> validGender.name().equalsIgnoreCase(gender.toString()));
     }
