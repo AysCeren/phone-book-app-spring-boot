007bd6b31024ccc621ab0526ced90b31ac34e5ee
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-02-28 16:53:52 +0300

    logging için logback.xml yeniden yazıldı WebConfig ve LoggingInterceptor eklendi Ana filtreleme işlemi LoggingFilter.java kullanılıyor

---
+++ src/main/java/com/project/contactsdemo/loggingfilter/WebConfig.java
@@ -0,0 +1,21 @@
+package com.project.contactsdemo.loggingfilter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private LoggingInterceptor loggingInterceptor;
+    @Autowired
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
