dc209e40dd6fca899886a5ba737148e7ed023b30
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-03-07 11:40:54 +0300

    mesaj basma şekli değiştirildi --> period methodu kaldırıldı

--- src/main/java/com/project/contactsdemo/validation/BirthdateValidator.java
+++ src/main/java/com/project/contactsdemo/validation/BirthdateValidator.java
@@ -1,8 +1,8 @@
 package com.project.contactsdemo.validation;
 
+import com.project.contactsdemo.exception.ValidationControlException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import jakarta.validation.ValidationException;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -14,19 +14,22 @@ public class BirthdateValidator implements ConstraintValidator<ValidBirthdate, S
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @Override
-    public boolean isValid(String birthDateStr, ConstraintValidatorContext context) {
+    public boolean isValid(String birthDateStr, ConstraintValidatorContext context) throws ValidationControlException {
         if (birthDateStr == null || birthDateStr.trim().isEmpty()) {
-            throw new ValidationException("Birthdate cannot be empty"); // Birthdate cannot be null or empty
+            throw new ValidationControlException("Birthdate cannot be empty"); // Birthdate cannot be null or empty
         }
-
         try {
             LocalDate birthDate = LocalDate.parse(birthDateStr, FORMATTER);
             LocalDate today = LocalDate.now();
 
             // Ensure the birthdate is in the past and the person are at least 18 years old
-            return !birthDate.isAfter(today) && Period.between(birthDate, today).getYears() >= MIN_AGE;
+           if(birthDate.isAfter(today))
+               throw new ValidationControlException("Birthdate must be after today");
+           else if(Period.between(birthDate, today).getYears() >= MIN_AGE)
+               throw new ValidationControlException("Birthdate must be between " + MIN_AGE);
+            return true;
         } catch (DateTimeParseException e) {
-            throw new ValidationException("Doğum tarihi şimdiki zamandan büyük olamaz"); // Invalid date format
+            throw new ValidationControlException("Please enter valid date, control your month and year"); // Invalid date format
         }
     }
 }
