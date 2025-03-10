a88c34941ab7eae3d62beb14abcc085ab40d3758
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-03-10 10:04:58 +0300

    return false dönmek yerine throw ValidationControlException atıldı

--- src/main/java/com/project/contactsdemo/validation/PhoneNumberValidator.java
+++ src/main/java/com/project/contactsdemo/validation/PhoneNumberValidator.java
@@ -5,6 +5,7 @@ import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import com.project.contactsdemo.exception.ValidationControlException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +15,13 @@ public class PhoneNumberValidator implements ConstraintValidator<ValidPhoneNumbe
     @Override
     public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
         PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
-        if (phoneNumber == null) return true; // Allow null values
-
+        if (phoneNumber == null) return true; // Allow null values -->belki de bu null durumunu kontrole gerek yoktur
         try {
             Phonenumber.PhoneNumber phone = phoneNumberUtil.parse(phoneNumber, "TR"); // Change default country as needed
             return phoneNumberUtil.isValidNumber(phone);
         } catch (NumberParseException e) {
             log.error(e.getMessage());
-            return false;
+            throw new ValidationControlException("Invalid phone number");
         }
     }
 }
