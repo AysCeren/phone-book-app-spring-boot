180c24f5e12646ac42e32689f35603f0857691ea
16381136672 <ayse.ceren.coban@gelirler.gov.tr>
2025-03-10 10:06:55 +0300

    enum için getGender eklendi-> null ya da Gender variable dönüşlü bir method. GenderValidator getGender'ı kullanıyor

--- src/main/java/com/project/contactsdemo/enums/Gender.java
+++ src/main/java/com/project/contactsdemo/enums/Gender.java
@@ -1,8 +1,18 @@
 package com.project.contactsdemo.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum Gender {
     //Enum type will be String
     MALE,
     FEMALE,
     UNKNOWN;
+
+    public static Gender getGender(String gender) {
+        return Arrays.stream(Gender.values()).filter(value -> value.toString().equalsIgnoreCase(gender))
+                .findFirst().orElse(null);
+    }
 }
