9d663ad493e39d047050e21a86c3495a997039e0
ayse.ceren.coban <ayse.ceren.coban@gelirler.gov.tr>
2025-04-10 10:52:42 +0300

    unutulan bir methoda unutulan bir caching mechanism eklendi

--- src/main/java/com/project/contactsdemo/contact/service/GetAllContactService.java
+++ src/main/java/com/project/contactsdemo/contact/service/GetAllContactService.java
@@ -4,8 +4,10 @@ import com.project.contactsdemo.contact.dto.ContactResponseDTO;
 import com.project.contactsdemo.contact.entity.Contact;
 import com.project.contactsdemo.contact.mapper.ContactMapper;
 import com.project.contactsdemo.contact.repository.ContactRepository;
+import com.project.contactsdemo.core.cache.CacheService;
 import com.project.contactsdemo.core.dto.GenericDTO;
 import com.project.contactsdemo.core.exception.NoDataFoundException;
+import com.project.contactsdemo.person.dto.PersonResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -15,9 +17,19 @@ import java.util.List;
 public class GetAllContactService {
     private final ContactRepository contactRepository;
     private final ContactMapper contactMapper;
+    private final CacheService cacheService;
 
     public GenericDTO<List<ContactResponseDTO>> getAllContactDTO() {
 
+        String key = "contactResponseAll";
+        String mapName = "contactResponseAll";
+        List<ContactResponseDTO> fromCache = (List<ContactResponseDTO>) cacheService.getFromCache(key,mapName);
+        if((fromCache != null && fromCache.size() != 0)) {
+            GenericDTO<List<ContactResponseDTO>> genericDTO = new GenericDTO<>(0,null);
+            genericDTO.setBody(fromCache);
+            return genericDTO;
+        }
+        //else: eğer Cache'de yoksa önce repoya gidelim
         List<Contact> contactList = contactRepository.findAll();
         if (contactList.isEmpty()) {
             throw new NoDataFoundException("There is no person found");
@@ -29,6 +41,7 @@ public class GetAllContactService {
                 .map(contactMapper::fromContactEntityToContactResponseDTO)
                 .toList();
         genericDTO.setBody(allContact);
+        cacheService.saveToCache(allContact,key,mapName);
         return genericDTO;
     }
 }
